bda90131a99a92fad9d39e395316c3456dc84458
Kim LaRocca <kim@kimlarocca.com>
2020-10-07 11:46:59 -0400

    boilerplate for 404 and other errors

---
+++ layouts/error.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="l-container">
+    <h1 v-if="error.statusCode === 404">404 page not found</h1>
+    <h1 v-else>An error occurred</h1>
+    <nuxt-link to="/">Go back to the home page</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error']
+}
+</script>
