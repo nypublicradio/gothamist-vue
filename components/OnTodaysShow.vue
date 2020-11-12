db4fbbbe3fdedd41753f09580f4ffe6a1eb162a0
Kim LaRocca <kim@kimlarocca.com>
2020-11-12 11:45:43 -0500

    finishing up on today's show

---
+++ components/OnTodaysShow.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="on-todays-show l-grid l-grid--2up l-grid--1up--large l-grid--large-gutters">
+    <div class="on-todays-show-left l-grid--order-1-large">
+      <h2 class="on-todays-show-title">On Today's Show</h2>
+      <p class="on-todays-show-headline">The Future of Industry City; How Do Prosecutors Decide When to Convict Cops?; How the Kennedy Campaign Used Tech and Data</p>
+      <v-spacer size="triple" />
+      <segment-list>
+        <segment-list-item
+          v-for="(segment, index) in segments.slice(0, segmentsToShow)"
+          :key="index"
+          :title="segment.title"
+          :url="segment.url"
+        />
+        <v-button
+          v-if="segments.length > segmentsToShow"
+          label="show more"
+          class="u-space--top"
+          @click="segmentsToShow=segments.length"
+        />
+      </segment-list>
+    </div>
+    <div class="on-todays-show-right l-grid--order-2-large">
+      <image-with-caption
+        alt-text="image alt text"
+        image="http://placehold.it/506x327"
+        caption="This is the caption lorem ipsum dolor sit amet, consectetur adipiscing elit lorem ipsum dolor sit amet, consectetur adipiscing elitlorem ipsum dolor sit amet, consectetur adipiscing elit"
+        credit="( AP Photo/Carolyn Kaster )"
+        credit-url="http:///www.google.com"
+      />
+      <div class="dots" />
+    </div>
+  </div>
+</template>
+
+<script>
+import whatsOnNow from '@/mixins/whatsOnNow'
+
+export default {
+  name: 'OnTodaysShow',
+  components: {
+    ImageWithCaption: () => import('nypr-design-system-vue/src/components/ImageWithCaption'),
+    SegmentList: () => import('nypr-design-system-vue/src/components/SegmentList'),
+    SegmentListItem: () => import('nypr-design-system-vue/src/components/SegmentListItem'),
+    VButton: () => import('nypr-design-system-vue/src/components/VButton'),
+    VSpacer: () => import('nypr-design-system-vue/src/components/VSpacer')
+  },
+  mixins: [whatsOnNow],
+
+  data () {
+    return {
+      segments: [
+        {
+          title: 'A Lawsuit Demanding Reparations, 100 Years After the Tulsa Race Massacre',
+          url: 'http://www.google.com'
+        },
+        {
+          title: 'Item Number Two with no link!',
+          url: ''
+        },
+        {
+          title: 'Item number 3',
+          url: 'http://www.google.com'
+        },
+        {
+          title: 'Item number 4',
+          url: 'http://www.google.com'
+        },
+        {
+          title: 'Item number 5',
+          url: 'http://www.google.com'
+        },
+        {
+          title: 'Item number 6',
+          url: 'http://www.google.com'
+        },
+        {
+          title: 'Item number 7',
+          url: 'http://www.google.com'
+        },
+        {
+          title: 'Item number 8',
+          url: 'http://www.google.com'
+        },
+        {
+          title: 'Item number 9',
+          url: 'http://www.google.com'
+        },
+        {
+          title: 'Item number 10',
+          url: 'http://www.google.com'
+        }
+      ],
+      segmentsToShow: 6
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,43 @@
 <template>
-  <div class="l-container l-container--12col">
-    <lazy-hydrate ssr-only>
-      <h1 class="is-vishidden">
-        Livestream WNYC FM 93.9 and AM 820
-      </h1>
-    </lazy-hydrate>
-    <v-spacer size="quad" />
-    <stream-switcher class="u-color-group-dark">
-      <stream-switcher-card
-        v-for="(stream, index) in $store.getters['whatsOnNow/streams']"
-        :key="index"
-        :station="stream.station"
-        :title="stream.title"
-        :up-next-title="stream.upNextTitle"
-        :active="stream.active"
-        :playing="stream.playing"
-        @click="setSelectedStream(stream, index)"
-      />
-    </stream-switcher>
-    <main-player
-      :image="$store.getters['whatsOnNow/selectedStreamImage']"
-      :title="$store.getters['whatsOnNow/selectedStreamTitle']"
-      :title-link="$store.getters['whatsOnNow/selectedStreamTitleLink']"
-      :details="$store.getters['whatsOnNow/selectedStreamDetails']"
-      :details-link="$store.getters['whatsOnNow/selectedStreamDetailsLink']"
-      :time="$store.getters['whatsOnNow/selectedStreamTime']"
-    >
-      <v-button
-        label="Listen Live"
-        @click="togglePlay($store.getters['whatsOnNow/selectedStream'])"
+  <div class="l-container">
+    <div class="l-container--12col">
+      <lazy-hydrate ssr-only>
+        <h1 class="is-vishidden">
+          Livestream WNYC FM 93.9 and AM 820
+        </h1>
+      </lazy-hydrate>
+      <v-spacer size="quad" />
+      <stream-switcher class="u-color-group-dark">
+        <stream-switcher-card
+          v-for="(stream, index) in $store.getters['whatsOnNow/streams']"
+          :key="index"
+          :station="stream.station"
+          :title="stream.title"
+          :up-next-title="stream.upNextTitle"
+          :active="stream.active"
+          :playing="stream.playing"
+          @click="setSelectedStream(stream, index)"
+        />
+      </stream-switcher>
+      <main-player
+        :image="$store.getters['whatsOnNow/selectedStreamImage']"
+        :title="$store.getters['whatsOnNow/selectedStreamTitle']"
+        :title-link="$store.getters['whatsOnNow/selectedStreamTitleLink']"
+        :details="$store.getters['whatsOnNow/selectedStreamDetails']"
+        :details-link="$store.getters['whatsOnNow/selectedStreamDetailsLink']"
+        :time="$store.getters['whatsOnNow/selectedStreamTime']"
       >
-        <pause-icon v-if="$store.getters['whatsOnNow/selectedStreamPlaying']" />
-        <play-simple v-else />
-      </v-button>
-    </main-player>
+        <v-button
+          label="Listen Live"
+          @click="togglePlay($store.getters['whatsOnNow/selectedStream'])"
+        >
+          <pause-icon v-if="$store.getters['whatsOnNow/selectedStreamPlaying']" />
+          <play-simple v-else />
+        </v-button>
+      </main-player>
+    </div>
+    <v-spacer size="triple" />
+    <on-todays-show />
   </div>
 </template>
 
@@ -46,6 +50,7 @@ export default {
   components: {
     LazyHydrate,
     MainPlayer: () => import('nypr-design-system-vue/src/components/MainPlayer'),
+    OnTodaysShow: () => import('../components/OnTodaysShow'),
     PauseIcon: () => import('nypr-design-system-vue/src/components/icons/wqxr/PauseIcon'),
     PlaySimple: () => import('nypr-design-system-vue/src/components/icons/PlaySimple'),
     StreamSwitcher: () => import('nypr-design-system-vue/src/components/StreamSwitcher'),
