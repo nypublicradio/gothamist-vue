09816bde42ffa0585d0cb8f3062f057567e8c90c
Kim LaRocca <kim@kimlarocca.com>
2021-03-22 13:45:21 -0400

    open external links in new window

--- mixins/navigation.js
+++ mixins/navigation.js
@@ -3,12 +3,16 @@ export default {
     formatNavigationArray (array) {
       const formattedArray = []
       array.forEach(function (item) {
+        let newWindow = false
+        if (item.type === 'external_link') {
+          newWindow = true
+        }
         formattedArray.push(
           {
             type: item.type,
             text: item.value.title,
             url: item.value.url,
-            newWindow: false
+            newWindow
           }
         )
       })
