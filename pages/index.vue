9f7e9453a1a4057f911069f6725fc39f2049b467
msbeethoven <seorisachs@gmail.com>
2021-04-13 16:49:58 -0400

    Start of design system feature styling

--- pages/index.vue
+++ pages/index.vue
@@ -1,58 +1,83 @@
 <template>
-  <div>
+  <div v-if="homepageData">
     <div class="l-container">
       <v-spacer size="quad" />
-      <div class="feature-border">
-        <h2 class="c-featured-blocks__heading">
-          <span class="c-featured-blocks__heading-icon o-icon u-icon--s u-path-fill--quaternary">
-            <svg
-              role="img"
-              class="o-arrow-stylish-arrow-icon"
-              viewBox="3 3 18 18"
-              width="40"
-              height="40"
-              preserveAspectRatio="xMidYMid meet"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <title>arrow</title>
-              <path fill-rule="evenodd" clip-rule="evenodd" d="M13.236 5L19.836 7.736L21 8.906L14.772 15.128L16.884 17.24L6.174 19.04L5.01 17.894L3 10.232L6.552 11.702L13.236 5ZM5.898 17L13.416 15.71L11.214 14.792L17.886 8.126L13.5 6.326L6.816 12.986L4.614 12.062L5.898 17.006V17Z" fill="black" />
-            </svg>
-
-          </span>
-          Featured
-        </h2>
-        <div v-if="homepageData">
-          <ul>
-            <li v-for="(feature, index) in featuredShows" :key="index">
-              <br>
-              <v-card
-                show-gallery-icon
-                class="gothamist mod-small"
-                image="http://placehold.it/150x150"
-                :image-height="150"
-                :image-width="150"
-                :title="feature.title"
-                title-link="http://www.google.com"
-                :tags="[{'name': 'news','slug': 'news'},{'name': 'sponsored','slug': ''}]"
+      <div class="c-home__content-top u-section-spacing--wide l-container l-container--xl l-wrap">
+        <section class="c-featured-blocks o-section u-breakout">
+          <h2 class="c-featured-blocks__heading">
+            <span class="c-featured-blocks__heading-icon o-icon u-icon--s u-path-fill--quaternary">
+              <svg
+                role="img"
+                class="o-arrow-stylish-arrow-icon"
+                viewBox="3 3 18 18"
+                width="40"
+                height="40"
+                preserveAspectRatio="xMidYMid meet"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                <article-metadata
-                  publish-date="Jan 1, 2020 1:25PM"
-                  updated-date="Mar 2, 2020 10:08AM"
+                <title>arrow</title>
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M13.236 5L19.836 7.736L21 8.906L14.772 15.128L16.884 17.24L6.174 19.04L5.01 17.894L3 10.232L6.552 11.702L13.236 5ZM5.898 17L13.416 15.71L11.214 14.792L17.886 8.126L13.5 6.326L6.816 12.986L4.614 12.062L5.898 17.006V17Z" fill="black" />
+              </svg>
+            </span>
+            Featured
+          </h2>
+          <div class="l-grid l-grid--2x3 u-space--bottom">
+            <v-card
+              show-gallery-icon
+              class="gothamist mod-vertical mod-large"
+              image="http://placehold.it/640x430"
+              :image-height="150"
+              :image-width="150"
+              :title="featuredShows[0].title"
+              title-link="http://www.google.com"
+              subtitle="Here's what the latest Campaign Finance Board filings tell us."
+              :tags="[{'name': 'news','slug': 'news'},{'name': 'sponsored','slug': ''}]"
+            >
+              <article-metadata
+                publish-date="Jan 1, 2020 1:25PM"
+                updated-date="Mar 2, 2020 10:08AM"
+              >
+                <template v-slot:comments>
+                  <v-counter
+                    icon="comment"
+                    value="40"
+                    href="http://www.google.com"
+                  />
+                </template>
+              </article-metadata>
+            </v-card>
+            <!--- small featureds -->
+            <ul>
+              <li v-for="(feature, index) in featuredShows.slice(1,featuredShows.length)" :key="index">
+                <br>
+                <v-card
+                  show-gallery-icon
+                  class="gothamist mod-small"
+                  image="http://placehold.it/150x150"
+                  :image-height="150"
+                  :image-width="150"
+                  :title="feature.title"
+                  title-link="http://www.google.com"
+                  :tags="[{'name': 'news','slug': 'news'},{'name': 'sponsored','slug': ''}]"
                 >
-                  <template v-slot:comments>
-                    <v-counter
-                      icon="comment"
-                      value="40"
-                      href="http://www.google.com"
-                    />
-                  </template>
-                </article-metadata>
-              </v-card>
-            </li>
-          </ul>
-        </div>
-        <v-spacer size="quad" />
+                  <article-metadata
+                    publish-date="Jan 1, 2020 1:25PM"
+                    updated-date="Mar 2, 2020 10:08AM"
+                  >
+                    <template v-slot:comments>
+                      <v-counter
+                        icon="comment"
+                        value="40"
+                        href="http://www.google.com"
+                      />
+                    </template>
+                  </article-metadata>
+                </v-card>
+              </li>
+            </ul>
+          </div>
+        </section>
         <!-- news river -->
         <ul>
           <li v-for="(story, index) in nonFeaturedShows" :key="index">
@@ -82,10 +107,10 @@
             </v-card>
           </li>
         </ul>
+        <v-spacer size="quad" />
+        <read-more-in />
+        <v-spacer size="quad" />
       </div>
-      <v-spacer size="quad" />
-      <read-more-in />
-      <v-spacer size="quad" />
     </div>
   </div>
 </template>
@@ -107,14 +132,22 @@ export default {
   },
   computed: {
     featuredShows () {
-      return this.homepageData.items.filter((feature) => {
-        return feature.show_as_feature
-      }).slice(0, 4)
+      if (this.homepageData) {
+        return this.homepageData.items.filter((feature) => {
+          return feature.show_as_feature
+        }).slice(0, 4)
+      } else {
+        return null
+      }
     },
     nonFeaturedShows () {
-      return this.homepageData.items.filter((feature) => {
-        return !feature.show_as_feature
-      })
+      if (this.homepageData) {
+        return this.homepageData.items.filter((feature) => {
+          return !feature.show_as_feature
+        })
+      } else {
+        return null
+      }
     }
   },
   async mounted () {
@@ -128,7 +161,48 @@ export default {
 </script>
 
 <style lang="scss">
-.feature-border {
-  border: var(--border-standard);
+.l-container--xl {
+    max-width: var(--max-width-xl);
+    position: relative;
 }
+.c-featured-blocks {
+  padding: 50px;
+}
+
+.featured-stories {
+  display: flex;
+  background: pink;
+  flex-direction: column;
+}
+
+.big-stories {
+  flex-direction: column;
+  background:green;
+}
+
+.small-cards {
+  background: purple;
+  color: purple;
+  flex:1;
+  display: flex;
+}
+
+.c-featured-blocks__inner {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-start;
+}
+
+.c-featured-blocks__col1 {
+    max-width: 620px;
+    padding: 0 var(--space-3);
+    margin-bottom: -100px;
+    margin-right: var(--space-5);
+}
+
+.c-featured-blocks__col2 {
+    width: 380px;
+    margin-right: var(--space-5);
+}
+
 </style>
