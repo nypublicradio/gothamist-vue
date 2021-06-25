d67ac0e438211d2a80b9b05249d265f1a59fde3e
Kim LaRocca <kim@kimlarocca.com>
2021-06-24 20:51:31 -0400

    setup ability for redirects and added one for we the commuters

---
+++ middleware/redirects.js
@@ -0,0 +1,30 @@
+const redirects =
+  [
+    { from: '/tags/wethecommuters', to: '/tags/we-the-commuters' }
+  ]
+
+module.exports = function (req, res, next) {
+  // const host = req.headers.host
+  // const fullUrl = req.url
+  const url = req.url.split('?')[0]
+  let urlParams = null
+  if (req.url.includes('?')) {
+    urlParams = '?' + req.url.split('?')[1]
+  }
+
+  const redirect = redirects.find(r => r.from === url)
+  if (redirect) {
+    let newLocation
+    if (urlParams) {
+      newLocation = redirect.to + urlParams
+    } else {
+      newLocation = redirect.to
+    }
+    res.writeHead(301, {
+      Location: newLocation
+    })
+    res.end()
+  } else {
+    next()
+  }
+}
