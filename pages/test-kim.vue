09f2649bb2867d10531a7206a821f109f7a1cc95
Kim LaRocca <kim@kimlarocca.com>
2021-07-13 15:11:41 -0400

    article disqus feedback

---
+++ pages/test-kim.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <disqus-embed
+      :identifier="8645630297"
+      url="https://gothamist.com/news/cuomo-drops-plan-place-essential-workers-monument-near-irish-hunger-memorial"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'KimTestPage', // this is the template name which is used for GTM
+  components: {
+    DisqusEmbed: () => import('../components/DisqusEmbed')
+  }
+}
+</script>
