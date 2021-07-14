e31ff1ef742eb177e0c53ae9f1ebab3d71e4aec4
Kim LaRocca <kim@kimlarocca.com>
2021-07-14 12:36:13 -0400

    preview page

---
+++ components/InformationPage.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div
+      v-if="page"
+      class="l-container l-container--14col l-wrap"
+    >
+      <header class="c-basic__header u-spacing">
+        <h1 class="c-basic__heading o-3d-heading">
+          {{ page.title }}
+        </h1>
+      </header>
+      <v-spacer size="quad" />
+      <div class="information-page">
+        <v-streamfield :streamfield="page.body" />
+      </div>
+      <v-spacer size="quad" />
+    </div>
+  </div>
+</template>
+
+<script>
+import gtm from '@/mixins/gtm'
+
+export default {
+  name: 'InformationPage', // this is the template name which is used for GTM
+  components: {
+    VSpacer: () => import('nypr-design-system-vue/src/components/VSpacer'),
+    VStreamfield: () => import('../components/VStreamfield')
+  },
+  mixins: [gtm],
+  props: {
+    page: {
+      type: Object,
+      default: () => {
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.information-page p, .information-page h2 {
+  margin-bottom: var(--space-3);
+}
+
+.information-page {
+  max-width: var(--article-content-width);
+  margin: auto;
+}
+</style>
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,43 +1,22 @@
 <template>
   <div>
-    <div
-      v-if="contactPage"
-      class="l-container l-container--14col l-wrap"
-    >
-      <header class="c-basic__header u-spacing">
-        <h1 class="c-basic__heading o-3d-heading">
-          {{ contactPage.title }}
-        </h1>
-      </header>
-      <v-spacer size="quad" />
-      <div class="contact-page">
-        <v-streamfield :streamfield="contactPage.body" />
-      </div>
-      <v-spacer size="quad" />
-    </div>
+    <information-page :page="contactPage" />
   </div>
 </template>
 
 <script>
 import gtm from '@/mixins/gtm'
-import { mapState } from 'vuex'
 
 export default {
   name: 'Information', // this is the template name which is used for GTM
   components: {
-    VSpacer: () => import('nypr-design-system-vue/src/components/VSpacer'),
-    VStreamfield: () => import('../components/VStreamfield')
+    InformationPage: () => import('../components/InformationPage')
   },
   mixins: [gtm],
   data () {
     return {
       contactPage: null
     }
-  },
-  computed: {
-    ...mapState('global', {
-      donateUrl: state => state.donateUrl
-    })
   },
   async mounted () {
     await this.$axios
@@ -80,18 +59,3 @@ export default {
   }
 }
 </script>
-
-<style lang="scss">
-.u-padding--quad--top {
-  padding-top: calc(var(--space-3) * 3);
-}
-
-.contact-page p, .contact-page h2 {
-  margin-bottom: var(--space-3);
-}
-
-.contact-page {
-  max-width: var(--article-content-width);
-  margin: auto;
-}
-</style>
