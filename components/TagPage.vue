503e05a175fa72964e35198f2abb0cf090db85d7
walsh9 <walsh9@gmail.com>
2021-07-30 11:42:15 -0400

    Remove wrapper for breakout ads

--- components/TagPage.vue
+++ components/TagPage.vue
@@ -119,12 +119,8 @@
               class="u-border-accent u-hide-until--m u-space--double--bottom"
               aria-hidden="true"
             >
-            <div class="ad-wrapper-outer">
-              <div class="ad-wrapper-inner">
-                <div v-if="nuggetIndex === 0" key="interior_midpage_1" class="htlad-interior_midpage_1 htl-ad mod-break-margins mod-ad-disclosure" />
-                <div v-else key="interior_midpage_repeating" class="htlad-interior_midpage_repeating htl-ad mod-break-margins mod-ad-disclosure" />
-              </div>
-            </div>
+            <div v-if="nuggetIndex === 0" key="interior_midpage_1" class="htlad-interior_midpage_1 htl-ad mod-break-margins mod-ad-disclosure" />
+            <div v-else key="interior_midpage_repeating" class="htlad-interior_midpage_repeating htl-ad mod-break-margins mod-ad-disclosure" />
             <hr
               class="u-border-accent u-hide-until--m u-space--double--top"
               aria-hidden="true"
