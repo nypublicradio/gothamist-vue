9d007953310fce8ce97cb2a1d9f10158109bd47d
walsh9 <walsh9@gmail.com>
2021-09-20 19:35:35 -0400

    Remove local variable that was replacing global htlbid

--- mixins/htl.js
+++ mixins/htl.js
@@ -1,19 +1,21 @@
+/* global htlbid */
+
 export const setTargeting = function (targetingParams) {
-  const htlbid = window.htlbid = window.htlbid || {}
+  window.htlbid = window.htlbid || {}
   htlbid.cmd = htlbid.cmd || []
   htlbid.cmd.push(() => {
     for (const key in targetingParams) {
-      window.htlbid.setTargeting(key, targetingParams[key])
+      htlbid.setTargeting(key, targetingParams[key])
     }
   })
 }
 
 export const clearTargeting = function (targetingKeys) {
-  const htlbid = window.htlbid = window.htlbid || {}
+  window.htlbid = window.htlbid || {}
   htlbid.cmd = htlbid.cmd || []
   htlbid.cmd.push(() => {
     targetingKeys.forEach((key) => {
-      window.htlbid.clearTargeting(key)
+      htlbid.clearTargeting(key)
     })
   })
 }
