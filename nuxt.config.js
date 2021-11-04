36cf4501da76d2b92ae9bcb1f76397868ddd880a
walsh9 <walsh9@gmail.com>
2021-11-04 11:57:11 -0400

    make env case-insensitive

--- nuxt.config.js
+++ nuxt.config.js
@@ -216,7 +216,7 @@ export default {
         src: './templates/robots.txt',
         dst: '../static/robots.txt',
         options: {
-          env: process.env.ENV
+          env: process.env.ENV.toLowerCase()
         }
       }
     ]
