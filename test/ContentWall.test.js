fcc961b65ca4f7639f028a8e67cf19ad77fb0832
walsh9 <walsh9@gmail.com>
2022-01-12 17:49:13 -0500

    add contentwall test

---
+++ test/ContentWall.test.js
@@ -0,0 +1,40 @@
+import { shallowMount } from '@vue/test-utils'
+import { toHaveNoViolations } from 'jest-axe'
+import ContentWall from '../components/ContentWall'
+import axe from './axe-helper'
+
+describe('ContentWall', () => {
+  let component
+
+  beforeEach(() => {
+    component = shallowMount(ContentWall, {
+      slots: {
+        full: '<div class="full">Content</div>',
+        leadin: '<div class="leadin">Co</div>'
+      },
+      scopedSlots: {
+        wall: '<button class="wall" v-on:click="props.dismiss">see full</button>'
+      }
+    })
+  })
+
+  it('Shows the leadin content', () => {
+    const leadin = component.find('.leadin')
+    expect(leadin.exists()).toBe(true)
+  })
+  it('Shows the wall', () => {
+    const wall = component.find('.wall')
+    expect(wall.exists()).toBe(true)
+  })
+  it('Hides the full content', () => {
+    const full = component.find('.full')
+    expect(full.exists()).toBe(false)
+  })
+
+  it('Shows the full content when you dismiss the wall', async () => {
+    const wall = component.find('.wall')
+    await wall.trigger('click')
+    const full = component.find('.full')
+    expect(full.exists()).toBe(true)
+  })
+})
