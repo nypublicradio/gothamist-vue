f532be467d41372b996fd3e56942bf6314a3ae38
walsh9 <walsh9@gmail.com>
2022-03-03 16:46:07 -0500

    do tracking with gtm instead

--- plugins/google-optimize-analytics.client.js
+++ plugins/google-optimize-analytics.client.js
@@ -1,9 +1,11 @@
 // set experiment id and variant in ga
 // https://github.com/nuxt-community/google-optimize-module/issues/50
-export default ({ $ga, $exp: { experimentID, $variantIndexes } }) => {
+export default ({ $gtm, $exp: { experimentID, $variantIndexes } }) => {
   if (!experimentID) {
     return
   }
-  const exp = experimentID + '.' + $variantIndexes.join('-')
-  $ga.set('exp', exp)
+  $gtm.push({
+    optimizeExperimentId: experimentID,
+    optimizeVariantId: $variantIndexes.join('-')
+  })
 }
