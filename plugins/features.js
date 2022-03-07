4f4999ec6a60cffce8a707cd8fd5255b221ddca6
walsh9 <walsh9@gmail.com>
2022-03-07 17:55:38 -0500

    let me check for a disabled feature instead of an enabled one

--- plugins/features.js
+++ plugins/features.js
@@ -3,21 +3,43 @@ The features layer lets us decouple feature logic from experiment logic.
 Make your in-app conditionals based on the feature toggle instead of on the
 experiment itself
 
-$features.$enabled - object containing feature names as keys and a boolean value: true (enabled) / false (disabled),
+$features.enabled - object containing feature names as keys and a boolean value: true (enabled) / false (disabled),
   this is where we add new feature toggles
-$features.$classes - array of css classes, add these to the html body: feature-name-enabled or feature-name-disabled
+$features.disabled - the inverse of disabled, sometimes it's can be nicer to write like this
+$features.classes - array of css classes, add these to the html body: feature-name-enabled or feature-name-disabled
 
 This allows us to do things like:
 - make experiments that enable multiple features
 - toggle features based on logic that looks at query strings or build options
 - quickly enable a feature after an experiment
+
+### Using in css
+.feature-name-enabled .some-other-class {
+ --- some custom css ---
+}
+
+### Using in a template
+<div "#some-div-to-hide" v-if="!$features.enabled['feature-name]">
+</div>
+
+### using in script
+mounted () {
+  if (this.$features.enabled['feature-name']) {
+    --- code ---
+  }
+}
+
 */
 
 export default function ({ $exp }, inject) {
-  const $enabled = {
+  const enabled = {
     'tagless-homepage': $exp && $exp.name === 'test' && $exp.$variantIndexes.includes(1)
   }
-  const $classes = Object.entries($enabled)
+  const disabled = {}
+  for (const feature of Object.entries(enabled)) {
+    disabled.push(feature[0], !!feature[1])
+  }
+  const classes = Object.entries(enabled)
     .map(entry => entry[1] ? entry[0] + '-enabled' : entry[0] + '-disabled')
-  inject('features', { $enabled, $classes })
+  inject('features', { enabled, disabled, classes })
 }
