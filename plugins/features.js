8232f26df824575ffb7169055c8155b30e0a398c
walsh9 <walsh9@gmail.com>
2022-03-10 16:49:52 -0500

    Update documentation

--- plugins/features.js
+++ plugins/features.js
@@ -1,70 +1,16 @@
-/**
-# Feature toggles
-
-The feature toggle plugin was created to enable us to toggle a/b test experiment features
-without the rest of the code needing to know anything about the a/b testing setup. The client code
-shouldn't need to know what experiment id #123 variant #2 means, it just needs to know if a feature is
-enabled or disabled.
-
-While this was created with a/b testing in mind it also has other applications:
-https://en.wikipedia.org/wiki/Feature_toggle
-
-## Properties
-
-$features.enabled - An object containing feature names as keys and an enabled value as a boolean: true (enabled) / false (disabled)
-  this is where we add new feature toggles
-$features.disabled - the inverse of disabled,
-$features.classes - array of css classes, add these to the html body: feature-name-enabled or feature-name-disabled
-
-## Using toggles within the app
-
-### Using in css
-.feature-name-enabled .some-other-class {
- --- some custom css ---
-}
-
-### Using in a template
-<div "#some-div-to-hide" v-if="!$features.enabled['feature-name]">
-</div>
-
-### using in script
-mounted () {
-  if (this.$features.enabled['feature-name']) {
-    --- code ---
-  }
-}
-
-## Adding a feature toggle
-
-First, decide if it makes sense for your feature to use a feature toggle. While there
-are several cases where they're useful, too many toggles will create technical debt.
-
-To add a new feature toggle, add your feature name and the condition to enable it to
-the "enabled" object below. Also add a description of the feature and your name to aid
-in cleaning up the toggle later
-
-### Adding an google optimize toggle
-
-Prefix your feature with: `experiment-`
-
-### Adding an environment variable based toggle
-Useful when you need to integrate a feature into the main branch but aren't ready to deploy yet.
-
-### Adding a query string based toggle
-Useful for doing a "soft launch" of a feature that you need to see in the production
-environment but aren't ready to release to all users.
-
-## Cleaning up feature toggles
-
-*/
+/** See features.md in this same folder for documentation **/
 
 export default function ({ $exp }, inject) {
-  const enabled = {
+  // Add your features here
+  const features = {
     // Remove tags from the home page cards
     // Part of a Google Optimize a/b test
     // - Matt Walsh
+    // Expires:
     'experiment-tagless-homepage': $exp && $exp.name === 'test' && $exp.$variantIndexes.includes(1)
   }
+
+  const enabled = features || {}
   const disabled = {}
   for (const feature of Object.entries(enabled)) {
     disabled[feature[0]] = !!feature[1]
