168253fa777cc6bd2cc9423868637725c1cda436
walsh9 <walsh9@gmail.com>
2022-03-14 14:19:01 -0400

    cleanup

--- plugins/features.js
+++ plugins/features.js
@@ -13,9 +13,9 @@ export default function ({ $exp }, inject) {
   const enabled = features || {}
   const disabled = {}
   for (const feature of Object.entries(enabled)) {
-    disabled[feature[0]] = !!feature[1]
+    disabled[feature[0]] = !feature[1]
   }
   const classes = Object.entries(enabled)
-    .map(entry => entry[1] ? entry[0] + '-enabled' : entry[0] + '-disabled')
+    .map(entry => entry[1] ? `${entry[0]}-enabled` : `${entry[0]}-disabled`)
   inject('features', { enabled, disabled, classes })
 }
