b6f87df2bbc31e96f2c6913df7fc88948bf6079e
walsh9 <walsh9@gmail.com>
2022-09-27 16:25:53 -0400

    update cookie settings

--- pages/beta-opt-in.vue
+++ pages/beta-opt-in.vue
@@ -6,8 +6,12 @@
 export default {
   beforeMount () {
     const targetPath = 'https://gothamist.com' + this.$route.query.target || ''
-    const oneMonth = 60 * 60 * 24 * 31
-    this.$cookies.set('betaLot', '1', { path: '/', maxAge: oneMonth })
+    const cookieOptions = {
+      secure: true,
+      httpOnly: true,
+      expires: 'Thu, 01 Dec 2022 05:00:00 GMT'
+    }
+    this.$cookies.set('betaLot', '1', cookieOptions)
     window.location.href = targetPath
   }
 }
